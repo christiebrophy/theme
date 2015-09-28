32195105e215e8ae7666526b548ec4078f298ad9
Christie Brophy <christie.brophy@gmail.com>
2015-09-28 15:20:56 -0700

    aws s3

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def about
+  end
+
+  def work
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/about'
 
+  get 'home/work'
+
+  get 'home/contact'
+
+root 'home#index'
+resources:home
 end
