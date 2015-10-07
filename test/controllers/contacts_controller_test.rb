07cc4402910adca361b896d1849b13d68996438e
Christie Brophy <christie.brophy@gmail.com>
2015-10-07 13:57:59 -0700

    db problem

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ContactsControllerControllerTest < ActionController::TestCase
+class ContactsControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
